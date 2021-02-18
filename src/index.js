f491e379a9333d9839f2dd4fce6d1de398179942
ezzum <lihach.ac@gmail.com>
2021-02-18 11:11:58 +0400

    add express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import App from "./app/app";
 import { Provider } from "react-redux";
 import store from "./app/redux/store";
 
 const app = (
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 );
 
